c940c2bfdf4f732c1c1dd9c695163050e7000582
Shahzaib0217 <shahzaib.khan0217@gmail.com>
2023-01-28 13:02:32 +0500

    nft added to opensea

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x946553db29d99315063588De01f56336bC6208B0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sk.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
