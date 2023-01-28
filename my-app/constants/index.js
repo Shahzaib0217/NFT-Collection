8ea1078639b7da65a7b48624e7287242ac5ae490
Shahzaib0217 <shahzaib.khan0217@gmail.com>
2023-01-28 13:11:57 +0500

    nft contract address updated

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -583,4 +583,4 @@ export const abi = [
     }
 ]
 
-export const NFT_CONTRACT_ADDRESS = "0x8eEe781BdBE786e0611A6F587F0912BAE51ec6Be"
+export const NFT_CONTRACT_ADDRESS = "0x9C4A8dC0fe61ddAdAb908AAf05b0f8b10aC94922"
